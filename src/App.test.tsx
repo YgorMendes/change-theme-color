d9b9a28364152ad4851804b942fb6e9b14de1702
Ygor Mendes <ygortmendes@gmail.com>
2021-07-30 23:37:59 -0300

    provide values

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { ThemeProvider } from './providers/Theme';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <ThemeProvider>
+      <App />
+    </ThemeProvider> 
+    );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
