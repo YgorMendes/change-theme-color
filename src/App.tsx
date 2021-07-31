266966c1064b146f19f61b9433c7544f5aba8d9e
Ygor Mendes <ygortmendes@gmail.com>
2021-07-30 23:40:43 -0300

    render home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
+import Home from './pages/Home';
+import { useThemeContext } from './providers/Theme';
 import  './styles/globalStyles.css';
 
 function App() {
+  const { theme } = useThemeContext();
   return (
-    <>
-    <div className='asd'>
+    <div className={`bg-${theme === 'dark'? 'black-200': 'white-100'} h-screen`}>
      <Header/>
+     <Home/>
     </div>
-    </>
   );
 }
 
